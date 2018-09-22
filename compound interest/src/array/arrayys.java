72a96654d28c54e96a6e502e28385b11b9819957
user <user@AkinladeJoseph>
2018-09-22 21:58:41 +0100

    third

--- compound interest/src/array/arrayys.java
+++ compound interest/src/array/arrayys.java
@@ -1,5 +1,17 @@
 package array;
 
 public class arrayys {
+	public static void main(String[] args) {
+		
+		System.out.println("Index\tValue");
+		
+		int joe[] = {32,33,34,35,36};
+		
+		for(int counter=0; counter<joe.length; counter++) {
+			System.out.println(counter + "\t" + joe[counter]);
+		}
+		
+	}
+	
 
 }
